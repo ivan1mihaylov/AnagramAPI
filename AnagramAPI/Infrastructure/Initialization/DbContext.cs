beb7d4a6a85ecd0d79a4ce9a88facd9ea5c9eac7
Ivan Mihaylov <ivan1mihaylov@hotmail.com>
2019-12-12 00:16:25 +0200

    Auto migration added

--- AnagramAPI/Infrastructure/Initialization/DbContext.cs
+++ AnagramAPI/Infrastructure/Initialization/DbContext.cs
@@ -4,13 +4,14 @@ using Entity.Contracts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace AnagramAPI.Infrastructure.Initialization
 {
     internal class DbContext : IServicesRegistration
     {
         /// <summary>
-        /// Register the different DbContexts
+        /// Register the different DbContexts and initialize them
         /// </summary>
         /// <param name="services">The service decriptors</param>
         /// <param name="configuration">API settings</param>
@@ -20,6 +21,19 @@ namespace AnagramAPI.Infrastructure.Initialization
             // Registration of the DbContext as transient service is neccessary for the use of db health checks.
             // There is a workaround by implementing the dbcontext healthchecks by myself but as this is not a actual API I wont waste time on that.
             services.AddTransient<IAnagramDbContext>(c => c.GetRequiredService<AnagramDbContext>());
+
+            //Migrate the database if UseAutoMigrations is set to true
+            if(configuration["UseAutoMigration"] == "true")
+            {
+                try
+                {
+                    services.BuildServiceProvider().GetService<AnagramDbContext>().Database.Migrate();
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
         }
     }
 }
--- AnagramAPI/Startup.cs
+++ AnagramAPI/Startup.cs
@@ -63,8 +63,6 @@ namespace AnagramAPI
                    config.MapHealthChecksUI();
                });
 
-
-
         }
     }
 }
